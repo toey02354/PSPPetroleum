295f89e928732c060f5b60376272c4f6f86cce1c
toey02354 <nattapoln02354@gmail.com>
2021-05-07 00:27:05 +0700

    .

--- myScript.js
+++ myScript.js
@@ -12,3 +12,30 @@ window.addEventListener("scroll", () => {
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
+
+function show_certificate() {
+  var cer = document.getElementById("button-certificate");
+  var hist = document.getElementById("button-history");
+  var about = document.getElementById("button-about");
+  cer.style.display = "block";
+  hist.style.display = "none";
+  about.style.display = "none";
+}
+
+function show_history() {
+  var cer = document.getElementById("button-certificate");
+  var hist = document.getElementById("button-history");
+  var about = document.getElementById("button-about");
+  cer.style.display = "none";
+  hist.style.display = "block";
+  about.style.display = "none";
+}
+
+function show_about() {
+  var cer = document.getElementById("button-certificate");
+  var hist = document.getElementById("button-history");
+  var about = document.getElementById("button-about");
+  cer.style.display = "noen";
+  hist.style.display = "none";
+  about.style.display = "block";
+}
